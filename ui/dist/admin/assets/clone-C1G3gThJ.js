f51c70b5a581608246b125e763539a6c76f7448e
admin <lalala123>
2026-07-05 18:42:52 +0800

    build: force-add pre-built frontend dist

---
+++ ui/dist/admin/assets/clone-C1G3gThJ.js
@@ -0,0 +1,1 @@
+import{aZ as r}from"./admin-ghSlit0I.js";var a=4;function n(o){return r(o,a)}export{n as c};
---
+++ ui/dist/admin/assets/swimlanesDiagram-G3AALYLV-D55uA3kz.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-CpyeNPTU.js";import{_ as a}from"./admin-ghSlit0I.js";import"./chunk-5VM5RSS4-B-yWU1_j.js";import"./chunk-XXDRQBXY-DvEffale.js";import"./chunk-VR4S4FIN-PR1dEYL7.js";import"./chunk-32BRIVSS-CLC5wPOD.js";import"./channel-Dkk7kcU8.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
